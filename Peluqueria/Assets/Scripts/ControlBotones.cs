79c29dcd3931409f215f22f7e3f0f80ac19e231e
Agustin Albornoz <valve_1960@hotmail.com>
2020-11-27 06:50:42 -0300

    Peluqueria
    Primer commit

---
+++ Peluqueria/Assets/Scripts/ControlBotones.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlBotones : MonoBehaviour
+{
+    public void Volver()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void Estilos()
+    {
+        SceneManager.LoadScene("Estilos");
+    }
+
+
+}
---
+++ Peluqueria/Assets/Scripts/ControlEstilos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlEstilos : MonoBehaviour
+{
+    public GameObject EstiloHombre;
+    public GameObject EstiloMujer;
+
+    public void ActivarHombre()
+    {
+        EstiloHombre.SetActive(true);
+        EstiloMujer.SetActive(false);
+    }
+
+    public void ActivarMujer()
+    {
+        EstiloMujer.SetActive(true);
+        EstiloHombre.SetActive(false);
+    }
+
+}
