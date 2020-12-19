2a627b6fce6cd6c68d25c4546dec5730900a56c7
Grandote473 <70612761+Grandote473@users.noreply.github.com>
2020-12-19 20:12:52 -0300

    3 Scene
    empeze la tercera 3 escena

---
+++ Peluqueria/Assets/Scripts/GirarCamara.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+
+public class GirarCamara : MonoBehaviour
+{
+    public float Speed;
+    
+    public void GirarDerecha()
+    {
+       
+        transform.Rotate(0, Speed * Time.deltaTime, 0);
+    }
+
+    public void GirarIzquierda()
+    {
+        transform.Rotate(0, -Speed * Time.deltaTime, 0);
+    }
+
+}
+
---
+++ Peluqueria/Assets/Scripts/GirarCamara2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+
+public class GirarCamara2 : MonoBehaviour
+{
+    public UnityEvent OnHold;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
